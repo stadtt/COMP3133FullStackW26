aed7701fec23526cca74679830b54137e30c2c5e
shaffaq <hai.shaffaq@gmail.com>
2026-03-13 17:51:28 -0400

    completed lab06

--- week09/lab06_101500534/exercise-4/customer.ts
+++ week09/lab06_101500534/exercise-4/customer.ts
@@ -2,20 +2,25 @@ export class Customer {
     
     private first_name: string | undefined;
     private last_name: string | undefined;
+    private age: number | undefined;
 
     public greeter(): void {
         console.log(`Hello, ${this.first_name} ${this.last_name}`);
     }
-    constructor(first_name: string, last_name: string) {
+    constructor(first_name: string, last_name: string, age: number) {
         this.first_name = first_name;
         this.last_name = last_name;
+        this.age = age;
+    }
+    public getAge(): number | undefined {
+        return this.age;
     }
 
 }
 
-let customer = new Customer("John", "Smith");
+let customer = new Customer("John", "Smith", 30);
 customer.greeter();
-
+console.log("Age: " + customer.getAge());
 
 
 
